64039a4b4c5beb3ad9b8c9ed28992185098a8f5b
Justin <201009224+diocletiana@users.noreply.github.com>
2026-04-27 14:06:30 -0400

    feat: rest models

--- mycompany/models.py
+++ mycompany/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Employee(models.Model):
     Fname = models.TextField()
@@ -13,9 +14,35 @@ class Employee(models.Model):
     Super_ssn = models.CharField(max_length=9)
     Dno = models.CharField()
 
+
 class Department:
     Dname = models.TextField()
     Dnumber = models.CharField(primary_key=True)
-    Mgr_ssn = models.CharField()
-    
+    Mgr_ssn = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    Mgr_start_date = models.CharField()
 
+
+class Dependent(models.Model):
+    Essn = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    Dependent_name = models.TextField()
+    Sex = models.TextField()
+    Bdate = models.CharField()
+    Relationship = models.TextField()
+
+
+class DEPT_LOCATIONS(models.Model):
+    Dnumber = models.ForeignKey(Department, on_delete=models.CASCADE)
+    Dlocation = models.TextField()
+
+
+class PROJECT(models.Model):
+    Pname = models.TextField()
+    Pnumber = models.CharField(primary_key=True)
+    Plocation = models.TextField()
+    Dnum = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+
+class WORKS_ON(models.Model):
+    Essn = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    Pno = models.ForeignKey(PROJECT, on_delete=models.CASCADE)
+    Hours = models.FloatField()
